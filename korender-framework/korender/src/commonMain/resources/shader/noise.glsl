07f4425ae8db4300df57cebe433f924a63cb0b4f
Alexander Zakusylo <zakgof@gmail.com>
2024-03-23 07:53:34 +0200

    Better fastcloudsky

--- korender-framework/korender/src/commonMain/resources/shader/noise.glsl
+++ korender-framework/korender/src/commonMain/resources/shader/noise.glsl
@@ -6,11 +6,11 @@ vec2 hash(vec2 p) {
 }
 
 float noise(in vec2 p) {
-    float K1 = 0.366025404; // (sqrt(3)-1)/2;
-    float K2 = 0.211324865; // (3-sqrt(3))/6;
+    float K1 = 0.366025404;// (sqrt(3)-1)/2;
+    float K2 = 0.211324865;// (3-sqrt(3))/6;
     vec2 i = floor(p + (p.x + p.y) * K1);
     vec2 a = p - i + (i.x + i.y) * K2;
-    vec2 o = (a.x > a.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0); //vec2 of = 0.5 + 0.5*vec2(sign(a.x-a.y), sign(a.y-a.x));
+    vec2 o = (a.x > a.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);//vec2 of = 0.5 + 0.5*vec2(sign(a.x-a.y), sign(a.y-a.x));
     vec2 b = a - o + K2;
     vec2 c = a - 1.0 + 2.0 * K2;
     vec3 h = max(0.5 - vec3(dot(a, a), dot(b, b), dot(c, c)), 0.0);
@@ -29,11 +29,15 @@ float fbm(vec2 n) {
 }
 
 float fbmTex(sampler2D noiseTexture, vec2 n) {
-    float total = 0.0, amplitude = 1.0;
+    float total = 0.0;
+    float amplitude = 1.0;
+    float freq = 1.0;
     for (int i = 0; i < 7; i++) {
-        total += (texture(noiseTexture, n).r-0.5) * amplitude;
+        total += (2.0 * texture(noiseTexture, n * freq).r - 1.0) * amplitude;
+
         n = msw * n;
         amplitude *= 0.2;
+        freq *= 2.0;
     }
     return total;
 }
