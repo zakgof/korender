6e7a3c4622996ec81afdc8d0a1c6b93becf4f143
Oleksandr Zakusylo <zakgof@gmail.com>
2024-04-04 19:55:20 +0300

    Fixed depth range

--- korender-framework/korender/src/commonMain/kotlin/projection/FrustumProjection.kt
+++ korender-framework/korender/src/commonMain/kotlin/projection/FrustumProjection.kt
@@ -12,7 +12,7 @@ class FrustumProjection(
     override val mat4 = Mat4(
         2.0f * near / width, 0f, 0f, 0f,
         0f, 2.0f * near / height, 0f, 0f,
-        0f, 0f, -far / (far - near), -far * near / (far - near),
+        0f, 0f, -2.0f / (far - near), -(far + near) / (far - near),
         0f, 0f, -1f, 0f
     )
 }
--- korender-framework/korender/src/commonMain/resources/shader/image.vert
+++ korender-framework/korender/src/commonMain/resources/shader/image.vert
@@ -9,5 +9,5 @@ uniform vec2 size;
 void main() {
     vtex = vec2(tex.x, 1.0 - tex.y);
     vec2 quad = pos + tex * size;
-    gl_Position = vec4((quad * 2.0) - 1.0, 0.0, 1.0);
+    gl_Position = vec4((quad * 2.0) - 1.0, -1.0, 1.0);
 }
