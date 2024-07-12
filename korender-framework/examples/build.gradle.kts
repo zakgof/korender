1a6fc7bb6b1300993cc0d5c99f3483495b4759fd
Oleksandr Zakusylo <zakgof@gmail.com>
2024-07-12 16:49:33 +0300

    Update icon and app name

--- korender-framework/examples/build.gradle.kts
+++ korender-framework/examples/build.gradle.kts
@@ -89,6 +89,7 @@ compose.desktop {
         mainClass = "com.zakgof.korender.MainKt"
 
         jvmArgs("--add-exports", "java.desktop/sun.awt=ALL-UNNAMED")
+
         nativeApplication {
         }
 
@@ -97,6 +98,12 @@ compose.desktop {
             packageName = "com.zakgof.korender"
             packageVersion = "0.1.1"
             modules("jdk.unsupported")
+            windows {
+                iconFile.set(project.file("korender32.ico"))
+            }
+            linux {
+                iconFile.set(project.file("korender32.png"))
+            }
         }
     }
 }
