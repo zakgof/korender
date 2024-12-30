b176f6d41983fbd696f6b5ac27e864d5d83f4b75
Oleksandr Zakusylo <zakgof@gmail.com>
2024-12-30 11:21:41 +0200

    Shadow example bug fix

--- korender-framework/examples/src/commonMain/kotlin/ShadowExample.kt
+++ korender-framework/examples/src/commonMain/kotlin/ShadowExample.kt
@@ -3,8 +3,8 @@ package com.zakgof.korender.examples
 import androidx.compose.runtime.Composable
 import com.zakgof.app.resources.Res
 import com.zakgof.korender.Korender
-import com.zakgof.korender.camera.DefaultCamera
 import com.zakgof.korender.StandartMaterialOption
+import com.zakgof.korender.camera.DefaultCamera
 import com.zakgof.korender.math.Transform.Companion.scale
 import com.zakgof.korender.math.Transform.Companion.translate
 import com.zakgof.korender.math.Vec3
@@ -24,7 +24,7 @@ fun ShadowExample() =
             Light(Vec3(1f, -1f, 1f).normalize())
             Camera(DefaultCamera(Vec3(-2.0f, 3f, 20f), -1.z, 1.y))
             Shadow {
-                Cascade(mapSize = 1024, near = 5.0f, far = 15.0f)
+                Cascade(mapSize = 1024, near = 10.0f, far = 25.0f)
             }
             Renderable(
                 materialModifier,
