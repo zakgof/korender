5ea58e007ea150c4c58b1ba848dd7529687e37aa
Oleksandr Zakusylo <zakgof@gmail.com>
2025-02-02 09:04:56 +0200

    Removed insecto and baker

--- insecto/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.jetbrainsCompose) apply false
-    alias(libs.plugins.kotlinMultiplatform) apply false
-    alias(libs.plugins.composeCompiler) apply false
-}
