6eec44ffd85a1e07db954ebddfa403177fe8b320
Oleksandr Zakusylo <zakgof@gmail.com>
2025-02-15 14:48:50 +0200

    ROAM terrain POC

---
+++ korender-framework/examples/src/commonMain/kotlin/Roam.kt
@@ -0,0 +1,167 @@
+package com.zakgof.korender.examples
+
+import com.zakgof.korender.math.Vec3
+
+class Roam(val levels: Int, val tileSize: Int, val height: (Float, Float) -> Float) {
+
+    val tiles = mutableSetOf<Tile>()
+    val mergeables = mutableSetOf<Tile>()
+
+    private val minMaxY = mutableMapOf<Tile, Pair<Float, Float>>()
+
+    init {
+        precalculateMaxDelta()
+        tiles += Tile(0, 0, levels)
+    }
+
+    private fun precalculateMaxDelta() {
+        for (l in 0..levels) {
+            l.tiles().forEach {
+                minMaxY[it] = if (l == 0) minMaxTile(it.x, it.z) else minMaxChildren(it)
+            }
+        }
+    }
+
+    private fun minMaxTile(x: Int, z: Int): Pair<Float, Float> {
+        var min = Float.MAX_VALUE
+        var max = Float.MIN_VALUE
+        for (xx in 0 until tileSize) {
+            for (zz in 0 until tileSize) {
+                val h = height((x * tileSize + xx).toFloat(), (z * tileSize + zz).toFloat())
+                if (h < min) min = h
+                if (h > max) max = h
+            }
+        }
+        return min to max
+    }
+
+    private fun minMaxChildren(tile: Tile): Pair<Float, Float> {
+        val ch = tile.children()
+        return ch.minOf { minMaxY[it]!!.first } to ch.maxOf { minMaxY[it]!!.second }
+    }
+
+    fun update(position: Vec3) {
+
+        val cache = HashMap<Tile, Float>()
+        fun pri(tile: Tile) = cache.getOrPut(tile) { tile.size() / (tile.distanceTo(position) + 0.001f) }
+
+        val splitQ = PriorityQueue<Tile> { -pri(it) }
+        val mergeQ = PriorityQueue<Tile> { pri(it) }
+
+        tiles.forEach { if (it.level > 0) splitQ.add(it) }
+        mergeables.forEach { mergeQ.add(it) }
+
+        while (true) {
+
+            val maxSplit = splitQ.peek()?.let { -pri(it) } ?: Float.NEGATIVE_INFINITY
+            val minMerge = mergeQ.peek()?.let { pri(it) } ?: Float.POSITIVE_INFINITY
+            val crazy = -maxSplit > minMerge
+
+            if (tiles.size in 61..64 && !crazy)
+                break
+
+            if (tiles.size > 65 || crazy) {
+                mergeQ.peek()?.let { merge(it, splitQ, mergeQ) }
+            }
+
+            if (tiles.size < 60) {
+                splitQ.peek()?.let { split(it, splitQ, mergeQ, true) }
+            }
+            break
+        }
+    }
+
+    private fun merge(tile: Tile, splitQ: PriorityQueue<Tile>, mergeQ: PriorityQueue<Tile>) {
+        mergeQ.remove(tile)
+        mergeables.remove(tile)
+        tile.children().forEach {
+            tiles.remove(it)
+            splitQ.remove(it)
+        }
+        tiles.add(tile)
+        if (tile.level > 0) splitQ.add(tile)
+        tile.parent()?.let { parent ->
+            if (tiles.containsAll(parent.children())) {
+                mergeables.add(parent)
+                mergeQ.add(parent)
+            }
+        }
+        tile.neighbors()
+            .mapNotNull { it.parent() }
+            .forEach { parent ->
+                if (tiles.containsAll(parent.children())) {
+                    mergeables.add(parent)
+                    mergeQ.add(parent)
+                }
+            }
+    }
+
+    private fun split(tile: Tile, splitQ: PriorityQueue<Tile>, mergeQ: PriorityQueue<Tile>, isMergeable: Boolean) {
+
+        println("Splitting $tile")
+
+        tiles.remove(tile)
+        splitQ.remove(tile)
+        if (isMergeable) {
+            mergeables.add(tile)
+            mergeQ.add(tile)
+        }
+        tile.parent()?.let {
+            mergeables.remove(it)
+            mergeQ.remove(it)
+        }
+        tile.children().forEach {
+            tiles.add(it)
+            if (it.level > 0) splitQ.add(it)
+        }
+        tile.neighbors()
+            .mapNotNull { it.parent() }
+            .filter { tiles.contains(it) }
+            .forEach {
+                println("... force splitting $it")
+                split(it, splitQ, mergeQ, false)
+            }
+    }
+
+
+    data class Tile(val x: Int, val z: Int, val level: Int) {
+        fun children() = listOf(
+            Tile(x, z, level - 1),
+            Tile(x + 1.shl(level - 1), z, level - 1),
+            Tile(x, z + 1.shl(level - 1), level - 1),
+            Tile(x + 1.shl(level - 1), z + 1.shl(level - 1), level - 1)
+        )
+
+
+        fun size() = 1.shl(level)
+
+        override fun toString(): String = "$level: $x,$z"
+    }
+
+    private fun Int.tiles(): List<Tile> = (0 until 1.shl(levels - this)).flatMap { x ->
+        (0 until 1.shl(levels - this)).map { z ->
+            Tile(x * 1.shl(this), z * 1.shl(this), this)
+        }
+    }
+
+    private fun Tile.distanceTo(position: Vec3): Float {
+        val npx = position.x.coerceIn((x * tileSize).toFloat(), ((x + 1.shl(level)) * tileSize).toFloat())
+        val npz = position.z.coerceIn((z * tileSize).toFloat(), ((z + 1.shl(level)) * tileSize).toFloat())
+        return (position.x - npx) * (position.x - npx) + (position.z - npz) * (position.z - npz)
+    }
+
+    private fun Tile.parent(): Tile? = if (level == levels)
+        null
+    else
+        Tile(x.shr(level + 1).shl(level + 1), z.shr(level + 1).shl(level + 1), level + 1)
+
+    private fun Tile.neighbors() = listOf(
+        Tile(x - size(), z, level),
+        Tile(x + size(), z, level),
+        Tile(x, z - size(), level),
+        Tile(x, z + size(), level)
+    ).filter { x >= 0 && z >= 0 && x < 1.shl(levels) && z < 1.shl(levels) }
+
+
+}
+
--- korender-framework/examples/src/commonMain/kotlin/TerrainExample.kt
+++ korender-framework/examples/src/commonMain/kotlin/TerrainExample.kt
@@ -7,11 +7,12 @@ import com.zakgof.korender.Attributes.POS
 import com.zakgof.korender.Attributes.TEX
 import com.zakgof.korender.Korender
 import com.zakgof.korender.math.ColorRGB
-import com.zakgof.korender.math.Transform.Companion.translate
+import com.zakgof.korender.math.Transform.Companion.scale
 import com.zakgof.korender.math.Vec3
 import com.zakgof.korender.math.y
 import com.zakgof.korender.math.z
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+import kotlin.math.sin
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
@@ -22,6 +23,7 @@ fun TerrainExample() =
             normalTexture = texture("texture/asphalt-normal.jpg")
             pbr.metallic = 0.2f
         }
+        val roam = Roam(8, 32) { x, z -> sin(x * 0.001f) + sin(z * 0.001f) * (1f - (x - 2048f) * (x - 2048f) / 2048f / 2048f) * (1f - (z - 2048f) * (z - 2048f) / 2048f / 2048f) }
         val mesh = customMesh("block", 33 * 33, 32 * 32 * 6, POS, NORMAL, TEX) {
             for (x in 0..32) {
                 for (z in 0..32) {
@@ -35,19 +37,23 @@ fun TerrainExample() =
                 }
             }
         }
-        camera = camera(Vec3(256*32*0.5f, 1024f, 256*32*0.5f), -1.y, 1.z)
-        projection = frustum(5f, 5f * height / width, 1f, 1200f)
+        camera = camera(Vec3(256 * 32 * 0.5f, 2048f, 256 * 32 * 0.5f), -1.y, 1.z)
+        projection = frustum(5f, 5f * height / width, 1f, 2200f)
         Frame {
+
+            camera = camera(Vec3(256 * 32 * 0.5f + frameInfo.time * 100f, 2048f, 256 * 32 * 0.5f + 150f), -1.y, 1.z)
+
             AmbientLight(ColorRGB.White)
-            for(x in 0 until 256) {
-                for(z in 0 until 256) {
-                    Renderable(materialModifier, mesh = mesh, transform = translate(Vec3(36f * x, 0f, 36f * z)))
-                }
+            roam.update(camera.position)
+            roam.tiles.forEach { tile ->
+                Renderable(materialModifier, mesh = mesh,
+                    transform = scale(1.shl(tile.level).toFloat(), 1f, 1.shl(tile.level).toFloat())
+                        .translate(Vec3(36f * tile.x, 0f, 36f * tile.z)))
             }
             Gui {
                 Column {
                     Filler()
-                    Text(id = "fps", text = "FPS ${frameInfo.avgFps}")
+                    Text(id = "fps", text = "TILES ${roam.tiles.size} FPS ${frameInfo.avgFps}")
                 }
             }
         }
