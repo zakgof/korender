2a955fbb9d93c06c623568078af15b1c5622d98b
Oleksandr Zakusylo <zakgof@gmail.com>
2025-07-05 16:48:59 +0300

    Migrate to Sonatype Central

--- korender-framework/settings.gradle.kts
+++ korender-framework/settings.gradle.kts
@@ -15,7 +15,6 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        maven("https://oss.sonatype.org/content/repositories/snapshots")
     }
 }
 
