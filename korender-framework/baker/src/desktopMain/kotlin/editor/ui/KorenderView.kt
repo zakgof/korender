3f52cc132e60cdb37f9d7421ff00b26095c5ca01
Oleksandr Zakusylo <zakgof@gmail.com>
2026-03-11 08:09:59 +0200

    Mouse drag 3d

--- korender-framework/baker/src/desktopMain/kotlin/editor/ui/KorenderView.kt
+++ korender-framework/baker/src/desktopMain/kotlin/editor/ui/KorenderView.kt
@@ -9,14 +9,52 @@ import com.zakgof.korender.MaterialModifier
 import com.zakgof.korender.TouchEvent
 import com.zakgof.korender.baker.resources.Res
 import com.zakgof.korender.context.FrameContext
+import com.zakgof.korender.context.KorenderContext
 import com.zakgof.korender.math.ColorRGB.Companion.white
 import com.zakgof.korender.math.ColorRGBA
+import com.zakgof.korender.math.Quaternion
 import com.zakgof.korender.math.Transform.Companion.translate
 import com.zakgof.korender.math.Vec3
 import editor.model.Material
 import editor.state.State
 import editor.state.StateHolder
+import editor.ui.TouchHandler.touch
 import editor.util.TextureImageCache
+
+object TouchHandler {
+
+    class Down(val touch: TouchEvent, val look: Vec3)
+
+    private var down: Down? = null
+
+    fun KorenderContext.screenToLook(e: TouchEvent): Vec3 {
+        val right = camera.direction.cross(camera.up)
+
+        val nx = (e.x + 0.5f) / width * 2f - 1f
+        val ny = 1f - (e.y + 0.5f) / height * 2f
+
+        return (camera.direction * projection.near +
+                right * (nx * projection.width * 0.5f) +
+                camera.up * (ny * projection.height * 0.5f)
+                ).normalize()
+    }
+
+    fun KorenderContext.touch(e: TouchEvent, holder: StateHolder) {
+        if (e.type == TouchEvent.Type.DOWN) {
+            val look = screenToLook(e)
+            holder.selectViaRay(look, e.keyboardModifiers.ctrlPressed)
+            down = Down(e, look)
+        }
+        if (e.type == TouchEvent.Type.MOVE && down != null) {
+            val newLook = screenToLook(e)
+            val rot = Quaternion.shortestArc(newLook, down!!.look)
+            holder.setCamera(camera.position, (rot * camera.direction).normalize(), (rot * camera.up).normalize())
+        }
+        if (e.type == TouchEvent.Type.UP) {
+            down = null
+        }
+    }
+}
 
 @Composable
 fun KorenderView(holder: StateHolder, focusHandler: () -> Unit) {
@@ -31,21 +69,7 @@ fun KorenderView(holder: StateHolder, focusHandler: () -> Unit) {
                 }
             }
         }
-        OnTouch {
-            if (it.type == TouchEvent.Type.DOWN) {
-                focusHandler()
-                val right = camera.direction.cross(camera.up)
-
-                val nx = (it.x + 0.5f) / width * 2f - 1f
-                val ny = 1f - (it.y + 0.5f) / height * 2f
-
-                val look = (camera.direction * projection.near +
-                            right * (nx * projection.width * 0.5f) +
-                            camera.up * (ny * projection.height * 0.5f)
-                            ).normalize()
-                holder.selectViaRay(look, it.keyboardModifiers.ctrlPressed)
-            }
-        }
+        OnTouch { touch(it, holder) }
         Frame {
             AmbientLight(white(0.6f))
             DirectionalLight(Vec3(1f, -1f, -1f), white(1.5f))
