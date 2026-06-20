c28c1fe9ee1472a25dc87f6b5b0da627d6af6d32
Oleksandr Zakusylo <zakgof@gmail.com>
2026-06-20 15:57:47 +0300

    Blur compile bug on Adreno

--- korender-framework/korender/src/commonMain/composeResources/files/shader/lib/blur.glsl
+++ korender-framework/korender/src/commonMain/composeResources/files/shader/lib/blur.glsl
@@ -7,8 +7,8 @@ vec3 blur(sampler2D tex, vec2 vtex, float radius, vec2 dir, float screen) {
     for (float i = 1.0; i <= radius; i++) {
         float dim = exp(- i * i / (radius * radius));
         w += dim * 2.0;
-        color += textureLod(tex, vtex + step * i, 0.0).rgb * dim;
-        color += textureLod(tex, vtex - step * i, 0.0).rgb * dim;
+        color += texture(tex, vtex + step * i).rgb * dim;
+        color += texture(tex, vtex - step * i).rgb * dim;
     }
     return color / w;
 }
@@ -19,8 +19,8 @@ float minDepthBlur(sampler2D tex, vec2 vtex, float radius, vec2 dir, float scree
     vec2 step = dir / screen;
 
     for (float i = 1.0; i <= radius; i++) {
-        depth = min(depth, textureLod(tex, vtex + step * i, 0.0).r);
-        depth = min(depth, textureLod(tex, vtex - step * i, 0.0).r);
+        depth = min(depth, texture(tex, vtex + step * i).r);
+        depth = min(depth, texture(tex, vtex - step * i).r);
     }
     return depth;
 }
